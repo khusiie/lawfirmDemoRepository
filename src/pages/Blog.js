66ae0b42c63e4aaf27bde9343cd13d7f9fa201b6
Khushi Gupta <khushi@example.com>
2025-07-15 02:26:37 +0530

    Initial commit

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TopHeader from '../components/topNavbar';
 import Navbar from '../components/Navbar';
-
 import Footer from '../components/Footer';
 import EndFooter from '../components/EndFooter';
 import HeroSection from '../components/blog/HeroSection';
